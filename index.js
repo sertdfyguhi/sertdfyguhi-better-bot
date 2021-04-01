99e192c7af27ed011796ba60a3613c26ec681532
sertdfyguhi <71869572+sertdfyguhi@users.noreply.github.com>
2021-04-01 11:11:48 +0800

    update code to be better

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-// this code is messy
-
 // todo:
-// add comments
+// nothing
 
 const discord = require('discord.js')
+
 const config = require('./config.json')
 const run = require('./run.js')
 
 const client = new discord.Client()
 
-client.on('ready', function(){
+client.on('ready', function () {
   console.log('logged in')
   client.user.setActivity('hey guys do s!help for help')
 })
 
-client.on('message', function(message){
+client.on('message', function (message) {
   if (message.content === `${config.prefix}help`) {
+    // help command
+
     const embed = new discord.MessageEmbed()
       .setColor('#3268a8')
       .setTitle('Commands')
@@ -28,6 +29,8 @@ client.on('message', function(message){
 
     message.channel.send(embed)
   } else if (message.content === `${config.prefix}langs`) {
+    // langs command
+
     let langs = ''
 
     for (const lang of run.langs) {
@@ -42,13 +45,14 @@ client.on('message', function(message){
 
     message.channel.send(embed)
   } else if (message.content.startsWith(`${config.prefix}code`)) {
-    const split = message.content.split(' ')
-    let code = message.content.substr(split[0].length + 2 + split[1].length)
-    let embed = new discord.MessageEmbed()
-      .setTitle('sertdfyguhi\'s code bot')
-      .setFooter('Requested by @' + message.author.tag)
+    // code command
 
-    let req;
+    const split = message.content.split(' ')
+
+    let code = message.content.substr(split[0].length + split[1].length + 2)
+    let embed = new discord.MessageEmbed()
+      .setTitle("sertdfyguhi's code bot")
+      .setFooter('Requested by @' + message.author.username)
 
     if (code.startsWith('\n')) {
       code = code.substring(1)
@@ -57,46 +61,57 @@ client.on('message', function(message){
     if (code.startsWith('```') && code.endsWith('```')) {
       const s = code.split('\n')
       s.pop()
-      s.shift()
+
+      s[s.length - 1] = s[s.length - 1].substring(
+        s[s.length - 1].indexOf('```')
+      )
 
       code = s.join('\n')
     }
 
-    if (run.langs.includes(split[1].toLowerCase())) {
-      const lang_lower = split[1].toLowerCase()
+    const lang_lower = split[1].toLowerCase()
 
-      req = run[lang_lower](code)
-      
-      if (!req.program_error && !req.compiler_error) {
+    if (run.langs.includes(lang_lower)) {
+      let res
+
+      res = run[lang_lower](code)
+
+      if (!res.program_error && !res.compiler_error) {
         if (code != '') {
-          embed.addField('Output', '```\n' + req.program_output + '```')
+          embed.addField('Output', '```\n' + res.program_output + '```')
         } else {
           embed.addField('Output', '``` ```')
         }
 
         embed.setColor('#4aff5c')
       } else {
-        if (!req.compiler_error) {
-          embed.addField('Output', '```\n' + req.program_error + '```')
+        if (!res.compiler_error) {
+          embed.addField('Output', '```\n' + res.program_error + '```')
         } else {
-          embed.addField('Output', '```\n' + req.compiler_error + '```')
+          embed.addField('Output', '```\n' + res.compiler_error + '```')
         }
 
         embed.setColor('#ff2b4f')
       }
 
-      embed.addField('permlink', req.url)
-      
+      embed.addField('permlink', res.url)
+
       if (embed.length > 2000) {
-        embed.fields[0].value = 'Program output is too long, please use permlink instead.'
+        embed.fields[0].value =
+          'Program output is too long, please use permlink instead.'
       }
 
       message.channel.send(embed)
     } else {
-      message.channel.send('Invalid language. Please do `.langs` for all the langauges.')
+      message.channel.send(
+        'Invalid language. Please do `.langs` for all the langauges.'
+      )
     }
   } else {
-    if (message.author != client.user && message.content.startsWith(config.prefix)) {
+    if (
+      message.author != client.user &&
+      message.content.startsWith(config.prefix)
+    ) {
       message.channel.send('Invalid command.')
     }
   }
